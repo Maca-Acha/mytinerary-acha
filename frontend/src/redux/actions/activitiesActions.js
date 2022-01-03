ea8c472eeeee9c9b61b76d34f90de4ce4bdbe435
Maca-Acha <achamariamacarena@gmail.com>
2022-01-03 16:48:55 -0300

    cambio localHost

--- frontend/src/redux/actions/activitiesActions.js
+++ frontend/src/redux/actions/activitiesActions.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const activitiesActions = {
     getActivities: () => {
         return async (dispatch, getState) => {
-            let response = await axios.get("http://localhost:4000/api/activities")
+            let response = await axios.get("https://mytinerary-acha.herokuapp.com/api/activities")
             dispatch({type: "GET_ACTIVITIES", payload: response.data.response})
         }}
 }
--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(passport.initialize())
 app.use("/api", Router)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"))
-  app.get("*", (req, res) => {
+    app.use(express.static("frontend/build"))
+    app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
-  })
+    })
 }
 
 app.listen(PORT, () =>
-  console.log("App listening on port " + PORT
+    console.log("App listening on port " + PORT
 )
 )
