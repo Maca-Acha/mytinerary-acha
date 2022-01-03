fd4ddf0e92495e9623aaa8547a0ac4a6f552fb4e
Maca-Acha <achamariamacarena@gmail.com>
2022-01-03 16:03:27 -0300

    make it better

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-require('dotenv').config()
+require("dotenv").config()
+require("./config/database")
 
-const Router = require('./routes/routes')
-const passport = require('passport')
 const express = require("express")
 const cors = require("cors")
+const Router = require("./routes/routes")
+const passport = require("passport")
 const app = express()
-require('./config/database')
+
+const PORT = process.env.PORT
 
 app.use(cors())
 app.use(express.json())
-app.use(passport.initialize());
-app.use('/api', Router)
+app.use(passport.initialize())
+app.use("/api", Router)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+  })
+}
 
-app.listen(4000,() => {console.log("Server listening on port 4000")})
+app.listen(PORT, () =>
+  console.log("App listening on port " + PORT
+)
+)
