1b8090198b920e9fce1339754d3edfbc87c5bb50
Maca-Acha <achamariamacarena@gmail.com>
2022-11-03 17:57:14 -0300

    build

--- index.js
+++ index.js
@@ -14,12 +14,11 @@ app.use(express.json())
 app.use(passport.initialize())
 app.use("/api", Router)
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("frontend/build"))
-    app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
-    })
-}
+
+app.use(express.static("frontend/build"))
+app.get("*", (req, res) => {
+res.sendFile(path.join(__dirname + "/frontend/build/index.html"))
+})
 
 app.listen(PORT, () =>
     console.log("App listening on port " + PORT
